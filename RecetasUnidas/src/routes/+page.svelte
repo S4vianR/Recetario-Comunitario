9266568daaae979543ef3e2d69e8b1150112e3fa
S4vianR <reuben4.com@gmail.com>
2024-04-11 10:21:36 -0600

    Update

--- RecetasUnidas/src/routes/+page.svelte
+++ RecetasUnidas/src/routes/+page.svelte
@@ -20,15 +20,21 @@
 		<section class="white right_pane">
 			<h2>Inicio de sesión</h2>
 			<form action="#" method="post">
-				<div>
-					<label for="username">Usuario</label>
-					<input type="text" name="usuario" id="username" />
-				</div>
-				<div>
-					<label for="password">Contraseña</label>
-					<input type="text" name="password" id="password" />
-				</div>
-				<button type="submit"> Iniciar sesión </button>
+				<section>
+					<div>
+						<label for="username">Usuario</label>
+						<input type="text" name="usuario" id="username" />
+					</div>
+					<div>
+						<label for="password">Contraseña</label>
+						<input type="text" name="password" id="password" />
+					</div>
+					<button type="submit"> Iniciar sesión</button>
+					<div id="mensaje_registro">
+						<p>¿No tienes una cuenta?</p>
+						<a href="/registro">¡Crea una ahora mismo!</a>
+					</div>
+				</section>
 			</form>
 		</section>
 	</section>
@@ -71,7 +77,17 @@
 		width: 60%;
 		display: flex;
 		flex-direction: column;
-		gap: 2rem;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.right_pane > form > section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: normal;
+		gap: 4rem;
+		width: fit-content;
 	}
 
 	.right_pane > form div {
@@ -91,10 +107,30 @@
 		outline: none;
 		border: 0.12rem solid #3f3f3f;
 		border-radius: 0.5rem;
-		width: 45%;
-		height: 2.1rem;
+		width: 25rem;
+		height: 2.575rem;
+		flex-shrink: 0;
 		padding: 0.5rem;
 		font-size: 1rem;
+	}
+
+	.right_pane > form button[type='submit'] {
+		border: 1px solid #000;
+		border-radius: 0.5rem;
+		background-color: #c99669;
+		height: 2rem;
+		width: fit-content;
+		font-size: 1rem;
+		padding: 0 1rem 0 1rem;
+		align-self: flex-end;
+	}
+
+	.right_pane > form #mensaje_registro {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		font-weight: 600;
 	}
 
 	.white {
