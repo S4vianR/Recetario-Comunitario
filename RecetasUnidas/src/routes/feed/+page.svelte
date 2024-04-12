67a3d65cbd27c49db703c6afb17350939167d7d2
S4vianR <reuben4.com@gmail.com>
2024-04-12 10:17:45 -0600

    Update

---
+++ RecetasUnidas/src/routes/feed/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import logo from '/src/lib/assets/logo-removebg.png';
+</script>
+
+<main>
+	<nav>
+		<img src={logo} alt="Logo" width="60" height="60" />
+	</nav>
+	Este es el feed principal
+</main>
+
+<style>
+	nav {
+		background-color: #3c8085;
+	}
+</style>
--- RecetasUnidas/src/routes/registro/+page.svelte
+++ RecetasUnidas/src/routes/registro/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	// Image imports
-	import vegetable_recipe_book from '/src/lib/assets/vegetable-recipe-book.png';
+	import food_stand_day from '/src/lib/assets/food-stand-day.png';
 	import logo from '/src/lib/assets/logo-removebg.png';
 
 	// Page title
@@ -14,30 +14,40 @@
 </head>
 <main>
 	<section>
-		<section class="purple left_pane">
+		<section class="green left_pane">
 			<img alt="" src={logo} class="logo" />
 		</section>
 		<section class="white right_pane">
-			<h2>Inicio de sesión</h2>
-			<form action="/feed" method="get">
+			<h2>Registro de usuario</h2>
+			<form action="#" method="post">
 				<section>
 					<div>
 						<label for="username">Usuario</label>
-						<input type="text" name="usuario" id="username" />
+						<input type="text" name="usuario" id="username" autocomplete="off" />
 					</div>
 					<div>
 						<label for="password">Contraseña</label>
-						<input type="text" name="password" id="password" />
+						<input type="text" name="password" id="password" autocomplete="off" />
 					</div>
-					<button type="submit"> Iniciar sesión</button>
-					<div id="mensaje_registro">
-						<p>¿No tienes una cuenta?</p>
-						<a href="/registro">¡Crea una ahora mismo!</a>
+					<div>
+						<label for="repeat_password">Repetir contraseña</label>
+						<input type="text" name="repeat_password" id="repeat_password" autocomplete="off" />
+					</div>
+					<div>
+						<label for="email">Correo</label>
+						<input type="text" name="email" id="email" autocomplete="off" />
+					</div>
+					<div class="footer-form">
+						<button type="submit"> Registrarte</button>
+						<div id="mensaje_registro">
+							<p>¿Ya tienes una cuenta?</p>
+							<a href="/">¡Ingresa ahora mismo!</a>
+						</div>
 					</div>
 				</section>
 			</form>
 		</section>
-		<img src={vegetable_recipe_book} alt="Vegetable Recipe Book" id="vegetable_recipe_book" />
+		<img src={food_stand_day} alt="Food Stand Day" id="vegetable_recipe_book" />
 	</section>
 </main>
 
@@ -77,7 +87,7 @@
 		border: 1px solid #000;
 		padding: 2rem;
 		width: 50%;
-		aspect-ratio: 3/2;
+		aspect-ratio: 4/3;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -95,14 +105,14 @@
 
 	.right_pane > form div {
 		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		gap: 0.5rem;
+		flex-direction: row;
+		justify-content: flex-end;
+		gap: 1rem;
 		align-items: center;
 	}
 
 	.right_pane > form label {
-		font-size: 1.5rem;
+		font-size: 1.1rem;
 		font-weight: 600;
 	}
 
@@ -130,11 +140,6 @@
 		align-self: flex-end;
 	}
 
-	.right_pane > form button[type='submit']:hover {
-		background-color: #c57b31;
-		cursor: pointer;
-	}
-
 	.right_pane > form #mensaje_registro {
 		display: flex;
 		flex-direction: row;
@@ -148,6 +153,12 @@
 		color: #801c02;
 	}
 
+	.right_pane > form .footer-form {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
+
 	#vegetable_recipe_book {
 		position: absolute;
 		left: 10rem;
@@ -159,7 +170,7 @@
 		background-color: #faf9f6;
 	}
 
-	.purple {
-		background-color: #9196da;
+	.green {
+		background-color: #236569;
 	}
 </style>
